9e6f73f423008590b2c87dcd84401cf4940be71d
Russel Porosky <russel@Russels-MacBook-Pro.local>
2022-01-09 14:20:45 -0700

    Update code style and documentation.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Coll
+
+_SHA256 Collision Finder_
+
+## What's This?
+
+I was curious how long it might take to find a collision for an SHA256 hash created from an arbitrary file on my computer (a 20MB PDF in this case). I don't care about making a file that still works, or about matching the size of the existing file, simply about how long it would take to create a sequence of bytes that would result in the same hash.
+
+This is a very naive implementation since it is only looking for uppercase letter sequences like "A", "B", ... "AA", "AB", etc.
+
+## Future Goals
+
+* expanded sequence, using all bytes from 0 to 254 (I believe 255 is an EOF signal) instead of just uppercase alphabet
+* multi-threading
+* improved visualization of progression
+* write the collision content to a file
+
+## About the Author
+
+I've been a developer for a lot of years, but this is my first exposure to Rust. I figured this would be an interesting project to try with it.
--- src/main.rs
+++ src/main.rs
@@ -6,33 +6,33 @@ use std::convert::TryFrom;
 use std::time::Instant;
 
 fn main() {
-    let now = Instant::now();
-    let target_hash = "a51b1fb1ca68e4b476c2bf2b3b6702a67880d75c7cd3f5c71bb8cf9d990d42c8";
-    println!("Checking for '{}'", target_hash);
+	let now = Instant::now();
+	let target_hash = "a51b1fb1ca68e4b476c2bf2b3b6702a67880d75c7cd3f5c71bb8cf9d990d42c8";
+	println!("Checking for '{}'", target_hash);
 
-    let mut counter: u128 = 0;
-    let mut last_length: u32 = 0;
-    let mut seq = SequenceBuilder::try_from("a")
-        .unwrap()
-        .build()
-        .unwrap();
-    let mut sha = Sha256::new();
+	let mut iterations: u128 = 0;
+	let mut last_length: u32 = 0;
+	let mut seq = SequenceBuilder::try_from("A")
+		.unwrap()
+		.build()
+		.unwrap();
+	let mut sha = Sha256::new();
 
-    loop {
-        let test_content = seq.to_string();
-        if last_length != test_content.chars().count() as u32 {
-            println!("Length {} in {} seconds, {} iterations checked.", last_length.to_formatted_string(&Locale::en), now.elapsed().as_secs().to_formatted_string(&Locale::en), counter.to_formatted_string(&Locale::en));
-            last_length = test_content.chars().count() as u32;
-        }
-        sha.input_str(test_content.as_str());
-        if sha.result_str() == target_hash {
-            // write test_content to a file
-            println!("FOUND SHA256 COLLISION IN {} SECONDS, {} ITERATIONS!", now.elapsed().as_secs().to_formatted_string(&Locale::en), counter.to_formatted_string(&Locale::en));
-            println!("{}", test_content.as_str());
-            break;
-        }
-        counter = counter + 1;
-        seq.next();
-        sha.reset();
-    }
+	loop {
+		let test_content = seq.to_string();
+		if last_length != test_content.chars().count() as u32 {
+			println!("Length {} in {} seconds, {} iterations checked.", last_length.to_formatted_string(&Locale::en), now.elapsed().as_secs().to_formatted_string(&Locale::en), iterations.to_formatted_string(&Locale::en));
+			last_length = test_content.chars().count() as u32;
+		}
+		sha.input_str(test_content.as_str());
+		if sha.result_str() == target_hash {
+			// write test_content to a file
+			println!("FOUND SHA256 COLLISION IN {} SECONDS, {} ITERATIONS!", now.elapsed().as_secs().to_formatted_string(&Locale::en), iterations.to_formatted_string(&Locale::en));
+			println!("{}", test_content.as_str());
+			break;
+		}
+		iterations = iterations + 1;
+		seq.next();
+		sha.reset();
+	}
 }
